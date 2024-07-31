b40cf4aa895ad3e21b2c08f121d9394d2b50a97f
KaikAp <kaik.silva4@outlook.com>
2024-07-31 18:57:47 -0300

    criação de diretorios

--- Exercicios/leitura de arquivos/src/main/Program.java
+++ Exercicios/leitura de arquivos/src/main/Program.java
@@ -1,25 +1,31 @@
 package main;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		String[] Lines = new String[] { "good morning", "good afternoon"};
-		String path = "B:\\out.txt";
+		Scanner sc = new Scanner(System.in);
 		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){ //o true verifica se ele já existe sem o parametro ele recria ou cria o arquivo
-			for(String line:Lines) {
-				bw.write(line);
-				bw.newLine();
-				
-			}
-			}catch(IOException e) {
-				e.printStackTrace();
-			}
+		System.out.println("digite uma pasta: ");
+		String strpath = sc.nextLine();
+		File path = new File(strpath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("Pastas: ");
+		for(File folder: folders) {
+			System.out.println(folder);
 		}
+		
+		boolean success = new File(strpath+ "\\subdir").mkdir();
+		
+		System.out.println("diretorio criado com sucesso" + success);
+		sc.close();
+	}
+	
+	
+	
 	}
 
