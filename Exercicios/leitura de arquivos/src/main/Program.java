6f6b95e7eab6d99d48ce55beb80896f0ee89dd78
KaikAp <kaik.silva4@outlook.com>
2024-07-31 12:16:33 -0300

    aula 1 de leitura de arquivos

---
+++ Exercicios/leitura de arquivos/src/main/Program.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File file = new File("B:\\WorkSpace\teste.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}catch(IOException e) {
+			System.out.println("error" + e.getMessage());
+		}finally{
+			if(sc != null) {
+			sc.close();
+			}
+		}
+	}
+
+}
